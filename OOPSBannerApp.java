68c0f622ab9dc0aeda622952e4a252185c13175b
Panya Singh <panyasingh98@gmail.com>
2026-02-23 05:44:25 +0530

    UC2 - Banner Print

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,16 @@
-public class OOPSBannerApp 
+public class OOPSBannerApp
 {
     public static void main(String[] args) 
     {
-        System.out.println("OOPS");
+
+        System.out.println("  ***      ***    ******    ******  ");
+        System.out.println(" ** **    ** **   **    **  **      ");
+        System.out.println("**   **  **   **  **    **  **      ");
+        System.out.println("**   **  **   **  ******     ****** ");
+        System.out.println("**   **  **   **  **              **");
+        System.out.println("**   **  **   **  **              **");
+        System.out.println(" ** **    ** **   **        **    **");
+        System.out.println("  ***      ***    **         ****** ");
+
     }
 }
